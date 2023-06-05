3f66cf1ea478837fe8e223dc049bb429a6e58a70
Victoria Lizzi <45150448+torikak11@users.noreply.github.com>
2023-06-05 19:28:26 +0100

    add analytics/vercel

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,5 @@
-import '@/styles/globals.css'
-import Footer from '@components/Footer';
+import "@/styles/globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata = {
   title: "Victoria Lizzi",
@@ -10,11 +10,15 @@ const RootLayout = ({ children }) => {
   return (
     <html leng="en">
       <head>
-        <link rel="icon" href="https://toriadev-portfolio.s3.us-west-2.amazonaws.com/portfolio-logo.png" />
+        <link
+          rel="icon"
+          href="https://toriadev-portfolio.s3.us-west-2.amazonaws.com/portfolio-logo.png"
+        />
       </head>
       <body className="bg-white">
         <main>
           {children}
+          <Analytics />
         </main>
       </body>
     </html>
